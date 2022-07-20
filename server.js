5c39b97f66d14f6bfc721cb9f62f475b49475145
kingofvibe <vibecity112@gmail.com>
2022-07-20 15:55:18 +0100

    added method to cors

--- server.js
+++ server.js
@@ -13,7 +13,10 @@ const commentRoute = require('./src/routes/commentsRoutes')
 // Middleware
 app.use(express.urlencoded({ extended: true })); 
 app.use(express.json());
-app.use(cors({ credentials: true, origin: "*" }));
+app.use(cors({ 
+     origin: "*", 
+   methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH']
+}));
 
 //router middleware
 app.use('/api/pay/',paystack)
