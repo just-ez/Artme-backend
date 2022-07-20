d5e1e810e09c370ec3346ac47eca8df60e188691
kingofvibe <vibecity112@gmail.com>
2022-07-20 16:28:59 +0100

    fixed cors error

--- server.js
+++ server.js
@@ -15,7 +15,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors({ 
      origin: "*", 
-   methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH']
+   methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH'],
+   allowedHeaders: 'Content-Type,Authorization'
 }));
 
 //router middleware
